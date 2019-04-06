87fa41f7fa839a19ba1f48516f1fd6bbf52c8408
kogamitsuhiro <utirn.ame@gmail.com>
2019-04-07 07:28:13 +0900

    storeの使用感を試すためにcouterとtodosを作成

---
+++ pages/counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <div>
+      <button @click="inc">
+        {{ value }}
+      </button>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  computed: {
+    value() {
+      return this.$store.state.counter.count
+    }
+  },
+  methods: {
+    inc: function () {
+      this.$store.commit(`counter/increment`, 1)
+    }
+  }
+}
+</script>
