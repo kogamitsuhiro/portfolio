5a7240e1ab7febe77417da689a2f956a96e77d89
kogamitsuhiro <utirn.ame@gmail.com>
2019-04-13 17:53:41 +0900

    contactページを作成した

---
+++ pages/contact/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-contact">
+    <section class="contact">
+      <h2 class="section-title">
+        コンタクト
+      </h2>
+      <ArticleBody>
+        <p>ポートフォリオをご覧いただきありがとうございます。<br>お問い合わせは以下より承ります。</p>
+      </ArticleBody>
+    </section>
+  </div>
+</template>
+
+<script>
+import ArticleBody from '~/components/common/ArticleBody'
+
+export default {
+  components: {
+    ArticleBody
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
